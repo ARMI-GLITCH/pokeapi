df281839f64f41552efb1cfb65baae10d00b7c03
armi-2077 <armigames109@gmail.com>
2024-05-05 15:17:26 -0600

    First Commit in Pokeapi

---
+++ pokeapi.py
@@ -0,0 +1,74 @@
+import requests
+import os
+import json
+import random
+
+# API Pokemon
+
+# Solicita respuesta de la API
+def api_pokemon():
+    os.system('clear')
+    print('󰟾' * 51)
+    print('󱎂-----󰐝------󰠰󰠰--POKEMON-BATTLE--󰠰󰠰------󰐝--------󱎂')
+    print('󰟾' * 51)
+    print('-PREPARANDO EL JUEGO , POR FAVOR ESPERE UN MOMENTO-')
+    print('󰟾' * 51)
+    response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=150")
+    list_pokemons = []
+    
+    if response.status_code == 200:
+        data = response.json()
+        pokemons_names = data["results"]
+        
+    for pokemon in pokemons_names:
+        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon['name']}")
+        pokemon_data = response.json()
+        name = pokemon_data['name']
+        if name:
+           list_pokemons.append(name)
+    return list_pokemons
+        
+           
+def pokemon_menu(list_pokemons):
+    game = False
+    while game == False:
+        choice_pokemons = []
+        for i , pokemon in enumerate(list_pokemons, 1):
+            print(f"{i}. {pokemon}")
+            print('󰟾' * 15)
+        choice = input('-Que deseas hacer?-[PALABRAS MAYUSCULAS PARA RESPONDER]\n'
+                       'A 󰛂 Elegir Pokemones Aleatorios\n'
+                       'B 󰛂 ELegir los Pokemones manualmente\n')
+        print('󰟾' * 15)
+        os.system('clear')
+        if choice == 'A':
+            new_pokemons = []
+            pokemon_1 = random.randint(1, 150)
+            pokemon_2 = random.randint(1 ,150)
+            pokemon_3 = random.randint(1, 150)
+            choice_pokemon_1 = list_pokemons[int(pokemon_1)  - 1]
+            choice_pokemon_2 = list_pokemons[int(pokemon_2)  - 1]
+            choice_pokemon_3 = list_pokemons[int(pokemon_3)  - 1]
+            new_pokemons.append(choice_pokemon_1)
+            new_pokemons.append(choice_pokemon_2)
+            new_pokemons.append(choice_pokemon_3)
+            print('󰟾' * 60)
+            print('󰛂 Haz elegido a {} , {} y {} para la batalla 󰛁'.format(choice_pokemon_1, choice_pokemon_2, choice_pokemon_3))
+            print('󰟾' * 60)
+            return new_pokemons            
+        if not choice:
+            input('[NO SELECCIONASTE NADA]-[ELIGE UNA RESPUESTA]\n'
+                  '-----------[ENTER PARA CONTINUAR]------------\n')
+            
+def pokemon_battle(new_pokemons):
+    print('Esta es tu lista de pokemones > ', new_pokemons)
+             
+            
+def main():
+    pokemons = api_pokemon()
+    list_pokemons = pokemon_menu(pokemons)
+    pokemon_battle(list_pokemons)
+    
+        
+if __name__ == '__main__':
+    main()
