14ca96181d4dc18cd72a0c19aa249dd379c1d953
armi-2077 <armigames109@gmail.com>
2024-05-09 13:54:45 -0600

    Starting the fight

--- pokeapi.py
+++ pokeapi.py
@@ -63,7 +63,7 @@ def pokemon_menu(list_pokemons):
                 for i , pokemon in enumerate(list_pokemons, 1):
                     print(f"{i}. {pokemon}")        
                     print('󰟾' * 15)
-            
+                print('')
                 print('Selecciona tu Pokemon , elige un Pokemon por su numero (1-150)\n'
                       '-------------[SOLO PUEDES ELEGIR 3 POKEMONES]-----------------')
                 try:
@@ -96,9 +96,13 @@ def pokemon_start(new_pokemons):
     sleep(10)
     input('[-------------[ENTER PARA CONTINUAR]--------------]')
     os.system('clear')
-    print('Estos son tus pokemones > ', new_pokemons)
     start = False
     while start == False:
+        print('󰟾' * 51)
+        print('󱎂-----󰐝------󰠰󰠰--POKEMON-BATTLE--󰠰󰠰------󰐝--------󱎂')
+        print('󰟾' * 51)
+        print('')
+        print('Estos son tus pokemones > ', new_pokemons)
         ready_or_not_ready = input('Estas listo para la pelea?\n'
                                    '-A-Si estoy listo\n'
                                    '-B-No estoy listo\n')
@@ -135,25 +139,89 @@ def pokemon_battle(new_pokemons , list_pokemons):
             print('Elegiste a > ', new_pokemons[1])
         elif your_choice == 3:
             print('Elegiste a > ', new_pokemons[2])
-            
-        random_pokemon = random.randint(0 , 146)
-        random_list = list_pokemons[random_pokemon]     
-        battle = input('Tu contrincante es : '+ random_list)
-        input('Turno de ' + random_list + ' [CPU]')
-        cpu_choice = random.randint(1, 3)
-        if cpu_choice == 1:
-           input(random_list + ' ataca con Golpe Fuerte y te resta 20 puntos de vida\n'
-                 '[--------------------[ENTER PARA CONTINUAR-----------------------]\n')
-        elif cpu_choice == 2:
-           input(random_list + ' ataca con Golpe Debil y te resta 10 puntos de vida\n'
-                 '[--------------------[ENTER PARA CONTINUAR-----------------------]\n')
-        elif cpu_choice == 3:
-           input(random_list + ' se regenera con Curacion Fuerte y le sube 30 puntos de vida\n'
-                 '[------------------------[ENTER PARA CONTINUAR---------------------------]\n')
-        elif cpu_choice == 4:
-            input(random_list + ' se regenera con Curacion Debil y le sube 15 puntos de vida\n'
-                 '[------------------------[ENTER PARA CONTINUAR----------------------------]\n')
         
+        cpu_pokemons = []
+        cpu_1 = random.randint(1, 146)
+        cpu_2 = random.randint(1 ,146)
+        cpu_3 = random.randint(1, 146)
+        choice_cpu_1 = list_pokemons[int(cpu_1)  - 1]
+        choice_cpu_2 = list_pokemons[int(cpu_2)  - 1]
+        choice_cpu_3 = list_pokemons[int(cpu_3)  - 1]
+        cpu_pokemons.append(choice_cpu_1)
+        cpu_pokemons.append(choice_cpu_2)
+        cpu_pokemons.append(choice_cpu_3)
+             
+        battle = input('Tu rivales son : {} , {} , {} '.format(choice_cpu_1, choice_cpu_2, choice_cpu_3))
+        start_of_the_game = False
+        while start_of_the_game == False:
+            if your_choice == 1:
+                pokemon_user_1 = new_pokemons[0]
+            elif your_choice == 2:
+                pokemon_user_2 = new_pokemons[1]
+            elif your_choice == 3:
+                pokemon_user_3 = new_pokemons[2]
+            enemy_pokemons = {
+                'pokemon_1' : choice_cpu_1,
+                choice_cpu_1 : 100,
+                'pokemon_2' : choice_cpu_2,
+                choice_cpu_2 : 100,
+                'pokemon_3' : choice_cpu_3,
+                choice_cpu_3 : 100
+            }
+            name_enemy_1 = enemy_pokemons['pokemon_1']
+            life_enemy_1 = enemy_pokemons[choice_cpu_1]
+            attack_1 = random.randint(1, 3)
+            print('Turno de > ', name_enemy_1 + ' [CPU]')
+            if attack_1 == 1:
+                print(name_enemy_1 + ' ataca con Golpe Fuerte y te resta 20 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            elif attack_1 == 2:
+                print(name_enemy_1 + ' ataca con Golpe Debil y te resta 10 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            elif attack_1 == 3:
+                print(name_enemy_1 + ' se regenera con Curacion instantanea y se cura 15 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            name_enemy_2 = enemy_pokemons['pokemon_2']
+            life_enemy_2 = enemy_pokemons[choice_cpu_2]
+            attack_2 = random.randint(1, 3)
+            print('Turno de > ', name_enemy_2)
+            if attack_2 == 1:
+                print(name_enemy_2 + ' ataca con Golpe Debil y te resta 5 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            elif attack_2 == 2:
+                print(name_enemy_2 + ' ataca con Golpe Fuerte y te resta 15 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            elif attack_2 == 3:
+                print(name_enemy_2 + ' se regenera con Curacion Debil y se cura 10 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            name_enemy_3 = enemy_pokemons['pokemon_3']
+            life_enemy_3 = enemy_pokemons[choice_cpu_3]
+            attack_3 = random.randint(1, 3)
+            print('Turno de > ', name_enemy_3)
+            if attack_3 == 1:
+                print(name_enemy_3 + ' ataca con Golpe Debil y te resta 25 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            elif attack_3 == 2:
+                print(name_enemy_3 + ' ataca con Golpe Fuerte y te resta 35 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            elif attack_3== 3:
+                print(name_enemy_3 + ' se regenera con Curacion y se cura 20 puntos de vida')
+                input('----------------------[ENTER PARA CONTINUAR]-------------------------')
+            input('Turno de > ' + pokemon_user_1 + " [TU]")
+            
+           
+            
+           
+            
+           
+            
+            # input('Hola')
+            
+            # input('Turno de ' + enemy_pokemons[choice] + ' [CPU]')
+            # cpu_choice = random.randint(1, 3)
+            
+            
+           
         
             
             
